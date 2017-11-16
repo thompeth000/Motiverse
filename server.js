17cee33bd6caeeb9bb2365e305b410893ae1bcd8
thompeth000@warren.k12.in.us <thompeth000@warren.k12.in.us>
2017-11-16 10:23:40 -0500

    Got Socket.io to work

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
-var app = require('express')();
-var http = require('http').Server(app);
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.use(express.static('resource'));
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
 });
 
 http.listen(3000, function(){
