703dcb40df3434ad965edb2af56c8eb32e82c3c2
unknown <thompeth000@warren.k12.in.us>
2018-04-13 09:28:30 -0400

    Automatic Commit

--- resource/dash.js
+++ resource/dash.js
@@ -10,10 +10,9 @@ function refreshUserData(){
  socket.emit('userQuery', {'username': 'dankMemes'});
  socket.on('getUser', function(data){
  document.getElementById('points').innerHTML = ('Points: ' + data.pts);
- document.getElementById('task1').innerHTML = ('Task 1: ' + data.tasks[1]);
- document.getElementById('task2').innerHTML = ('Task 2: ' + data.tasks[2]);
- document.getElementById('task3').innerHTML = ('Task 3: ' + data.tasks[3]);
- document.getElementById('task4').innerHTML = ('Task 4: ' + data.tasks[4]);
+ for(i = 0; i < data.tasks.length; i++){
+ document.getElementById('task' + (i + 1)).innerHTML = ('Task ' + (i + 1) + ': ' + data.tasks[0].title);
+}
 });
 }
 	  
